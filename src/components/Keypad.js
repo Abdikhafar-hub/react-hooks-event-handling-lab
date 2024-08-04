e09508933d5a023109245904575a8961c5f3a995
Abdikhafar <abdikhafarissack@gmail.com>
2024-08-04 14:58:54 +0300

    events

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,15 @@
 // Code Keypad Component Here
+import React from 'react';
 
 function Keypad (){
+    function handleChange(event){
+        console.log('Entering password...')
+    }
+    
     return (
-        <div></div>
+        <div>
+            <input type='text' onChange={handleChange} placeholder='fill here' />
+        </div>
     )
 }
 
